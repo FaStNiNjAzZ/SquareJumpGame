d0a216746f027e6a82d4d406d75987c99e70b6bf
FaStNiNjAzZ <47434948+FaStNiNjAzZ@users.noreply.github.com>
2019-11-11 16:30:23 +0000

    Started to make Porgress bar.

--- Square Jump/Assets/Scripts/Control Scripts/GameController.cs
+++ Square Jump/Assets/Scripts/Control Scripts/GameController.cs
@@ -1,22 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     public GameObject gameOverPanel;
     public GameObject endLevelPanel;
+    public Slider LevelProgressBar;
+    float LevelProgress1;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        LevelProgress1 = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        LevelProgressBar.value = Level1Progress();
     }
 
     public void GameOver()
@@ -34,7 +39,13 @@ public class GameController : MonoBehaviour
     {
         Invoke("ShowCompletePanel", 2.0f);
         Debug.Log("LevelComplete");
-        int LevelProgress1 = 1;
+        LevelProgress1 = 1.0f;
+
+    }
+
+    public void Level1Progress()
+    {
+        float progressBar = LevelProgress1;
     }
 
     public void ShowCompletePanel()
--- Square Jump/Assets/Scripts/Control Scripts/LevelSelectionControl.cs
+++ Square Jump/Assets/Scripts/Control Scripts/LevelSelectionControl.cs
@@ -25,4 +25,5 @@ public class LevelSelectionControl : MonoBehaviour
     {
         Application.LoadLevel("SampleScene");
     }
+
 }
