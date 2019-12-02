b9e9a630f84cf9ca96a2e66ef283cf5df5ebcf57
FaStNiNjAzZ <47434948+FaStNiNjAzZ@users.noreply.github.com>
2019-12-02 16:20:49 +0000

    Added new backgrounds for main menu and Betrayal of Fear, also added a pause menu.

---
+++ Square Jump/Assets/Scripts/Other Scripts/PauseMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    //Varibales
+    public static bool isGamePause = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isGamePause == true)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isGamePause = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0.0f;
+        isGamePause = true;
+
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("MainMenu");
+        isGamePause = false;
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene(Application.loadedLevelName);
+        isGamePause = false;
+    }
+}
